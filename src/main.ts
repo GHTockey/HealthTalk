f47c1f9b54da4c752b7344e9cf6aab587edfafe1
Tockey <tockey@yeah.net>
2023-06-06 10:53:41 +0800

    删除非多余文件，引入 Vant, 单位适配, css 变量主题定制

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import "vant/lib/index.css";
+import "./assets/styles/main.scss";
 
 const app = createApp(App)
 
